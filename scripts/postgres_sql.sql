a07161c6979c9267938a544b193dd9ae1667dabd
LeporidaeLepus <jingyuwang0499@gmail.com>
2024-04-26 21:23:07 -0400

    update dbschema

--- scripts/postgres_sql.sql
+++ scripts/postgres_sql.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA IF NOT EXISTS nyc_resturant_inspection;
+CREATE SCHEMA IF NOT EXISTS nyc_restaurant_inspection;
 
-CREATE  TABLE nyc_resturant_inspection.dim_date ( 
+CREATE  TABLE nyc_restaurant_inspection.dim_date ( 
 	date_id              bigint  NOT NULL  ,
 	"year"               integer    ,
 	quarter              integer    ,
@@ -14,13 +14,13 @@ CREATE  TABLE nyc_resturant_inspection.dim_date (
 	CONSTRAINT pk_dim_date PRIMARY KEY ( date_id )
  );
 
-CREATE  TABLE nyc_resturant_inspection.dim_inspection_type ( 
+CREATE  TABLE nyc_restaurant_inspection.dim_inspection_type ( 
 	inspection_id        bigint  NOT NULL  ,
 	inspection_type      varchar(255)    ,
 	CONSTRAINT pk_dim_inspection PRIMARY KEY ( inspection_id )
  );
 
-CREATE  TABLE nyc_resturant_inspection.dim_location ( 
+CREATE  TABLE nyc_restaurant_inspection.dim_location ( 
 	location_id          bigint  NOT NULL  ,
 	building             varchar(255)    ,
 	street               varchar(255)    ,
@@ -31,22 +31,22 @@ CREATE  TABLE nyc_resturant_inspection.dim_location (
 	CONSTRAINT pk_dim_location PRIMARY KEY ( location_id )
  );
 
-CREATE  TABLE nyc_resturant_inspection.dim_restaurant ( 
+CREATE  TABLE nyc_restaurant_inspection.dim_restaurant ( 
 	restaurant_id        bigint  NOT NULL  ,
 	restaurant_name      varchar(255)    ,
 	phone                text    ,
 	cuisine_description  varchar(255)    ,
-	CONSTRAINT pk_dim_resturant PRIMARY KEY ( restaurant_id )
+	CONSTRAINT pk_dim_restaurant PRIMARY KEY ( restaurant_id )
  );
 
-CREATE  TABLE nyc_resturant_inspection.dim_violation ( 
+CREATE  TABLE nyc_restaurant_inspection.dim_violation ( 
 	violation_id         bigint  NOT NULL  ,
 	violation_code       text  NOT NULL  ,
 	violation_description varchar(400)    ,
 	CONSTRAINT pk_dim_violation PRIMARY KEY ( violation_id )
  );
 
-CREATE  TABLE nyc_resturant_inspection.resturant_facts ( 
+CREATE  TABLE nyc_restaurant_inspection.restaurant_facts ( 
 	fact_id              bigint  NOT NULL  ,
 	inspection_score     integer    ,
 	yelp_star            numeric(2,1)    ,
@@ -54,10 +54,10 @@ CREATE  TABLE nyc_resturant_inspection.resturant_facts (
 	critical_flag        integer    ,
 	grade                integer    ,
 	locstion_id          bigint    ,
-	resturant_id         bigint    ,
+	restaurant_id        bigint    ,
 	inspection_id        bigint    ,
 	inspection_date_id   bigint    ,
 	violation_id         bigint    ,
 	grade_date_id        bigint    ,
-	CONSTRAINT pk_resturant_facts PRIMARY KEY ( fact_id )
- )
+	CONSTRAINT pk_restaurant_facts PRIMARY KEY ( fact_id )
+ ) 
