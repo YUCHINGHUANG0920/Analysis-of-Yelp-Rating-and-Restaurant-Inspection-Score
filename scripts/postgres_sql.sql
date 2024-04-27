58d30f037c404ebbd315a38e6b1c99f3fb3f177f
LeporidaeLepus <jingyuwang0499@gmail.com>
2024-04-26 21:11:58 -0400

    update dbschema and postgresSQL

--- scripts/postgres_sql.sql
+++ scripts/postgres_sql.sql
@@ -1,63 +1,63 @@
-CREATE SCHEMA IF NOT EXISTS nyc_restaurant_inspection;
+CREATE SCHEMA IF NOT EXISTS nyc_resturant_inspection;
 
-CREATE  TABLE nyc_restaurant_inspection.dim_date ( 
-	date_id              INT  NOT NULL  ,
-	"year"               INT    ,
-	quarter              INT    ,
-	"month"              INT    ,
-	"day"                INT    ,
+CREATE  TABLE nyc_resturant_inspection.dim_date ( 
+	date_id              bigint  NOT NULL  ,
+	"year"               integer    ,
+	quarter              integer    ,
+	"month"              integer    ,
+	"day"                integer    ,
 	month_name           varchar(100)    ,
 	day_name             varchar(100)    ,
-	week_of_the_year     INT    ,
-	week_of_the_month    INT    ,
+	week_of_the_year     integer    ,
+	week_of_the_month    integer    ,
 	date_iso_format      date    ,
 	CONSTRAINT pk_dim_date PRIMARY KEY ( date_id )
  );
 
-CREATE  TABLE nyc_restaurant_inspection.dim_inspection_type ( 
-	inspection_id        INT  NOT NULL  ,
+CREATE  TABLE nyc_resturant_inspection.dim_inspection_type ( 
+	inspection_id        bigint  NOT NULL  ,
 	inspection_type      varchar(255)    ,
 	CONSTRAINT pk_dim_inspection PRIMARY KEY ( inspection_id )
  );
 
-CREATE  TABLE nyc_restaurant_inspection.dim_location ( 
-	location_id          INT  NOT NULL  ,
+CREATE  TABLE nyc_resturant_inspection.dim_location ( 
+	location_id          bigint  NOT NULL  ,
 	building             varchar(255)    ,
 	street               varchar(255)    ,
-	zipcode              INT    ,
+	zipcode              integer    ,
 	borough              varchar(20)    ,
-	latitude             numeric(20,20)    ,
-	longitude            numeric(20,20)    ,
+	latitude             numeric(30,12)    ,
+	longitude            numeric(30,12)    ,
 	CONSTRAINT pk_dim_location PRIMARY KEY ( location_id )
  );
 
-CREATE  TABLE nyc_restaurant_inspection.dim_restaurant ( 
-	restaurant_id         INT  NOT NULL  ,
-	restaurant_name       varchar(255)    ,
-	phone                INT    ,
+CREATE  TABLE nyc_resturant_inspection.dim_restaurant ( 
+	restaurant_id        bigint  NOT NULL  ,
+	restaurant_name      varchar(255)    ,
+	phone                text    ,
 	cuisine_description  varchar(255)    ,
-	CONSTRAINT pk_dim_restaurant PRIMARY KEY ( restaurant_id )
+	CONSTRAINT pk_dim_resturant PRIMARY KEY ( restaurant_id )
  );
 
-CREATE  TABLE nyc_restaurant_inspection.dim_violation ( 
-	violation_id         INT  NOT NULL  ,
+CREATE  TABLE nyc_resturant_inspection.dim_violation ( 
+	violation_id         bigint  NOT NULL  ,
 	violation_code       text  NOT NULL  ,
-	violation_description varchar(255)    ,
+	violation_description varchar(400)    ,
 	CONSTRAINT pk_dim_violation PRIMARY KEY ( violation_id )
  );
 
-CREATE  TABLE nyc_restaurant_inspection.restaurant_facts ( 
-	fact_id              INT  NOT NULL  ,
-	inspection_score     INT    ,
-	yelp_star            INT    ,
-	yelp_review_count    INT    ,
-	critical_flag        INT    ,
-	grade                INT    ,
-	location_id          INT  NOT NULL  ,
-	restaurant_id         INT  NOT NULL  ,
-	inspection_id        INT  NOT NULL  ,
-	inspection_date_id   INT  NOT NULL  ,
-	violation_id         INT  NOT NULL  ,
-	grade_date_id        INT  NOT NULL  ,
-	CONSTRAINT pk_restaurant_facts PRIMARY KEY ( fact_id )
- );
+CREATE  TABLE nyc_resturant_inspection.resturant_facts ( 
+	fact_id              bigint  NOT NULL  ,
+	inspection_score     integer    ,
+	yelp_star            numeric(2,1)    ,
+	yelp_review_count    integer    ,
+	critical_flag        integer    ,
+	grade                integer    ,
+	locstion_id          bigint    ,
+	resturant_id         bigint    ,
+	inspection_id        bigint    ,
+	inspection_date_id   bigint    ,
+	violation_id         bigint    ,
+	grade_date_id        bigint    ,
+	CONSTRAINT pk_resturant_facts PRIMARY KEY ( fact_id )
+ )
