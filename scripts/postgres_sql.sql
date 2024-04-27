c0116dda24f7b212566f074c815ab28051e0e6a7
LeporidaeLepus <jingyuwang0499@gmail.com>
2024-04-26 20:18:26 -0400

    upload data to postgre. Issues need to be fixed.

--- scripts/postgres_sql.sql
+++ scripts/postgres_sql.sql
@@ -6,43 +6,43 @@ CREATE  TABLE nyc_resturant_inspection.dim_date (
 	quarter              INT    ,
 	"month"              INT    ,
 	"day"                INT    ,
-	month_name           VARCHAR(100)    ,
-	day_name             VARCHAR(100)    ,
+	month_name           varchar(100)    ,
+	day_name             varchar(100)    ,
 	week_of_the_year     INT    ,
 	week_of_the_month    INT    ,
-	date_iso_format      DATE    ,
+	date_iso_format      date    ,
 	CONSTRAINT pk_dim_date PRIMARY KEY ( date_id )
  );
 
 CREATE  TABLE nyc_resturant_inspection.dim_inspection_type ( 
 	inspection_id        INT  NOT NULL  ,
-	inspection_type      VARCHAR(255)    ,
+	inspection_type      varchar(255)    ,
 	CONSTRAINT pk_dim_inspection PRIMARY KEY ( inspection_id )
  );
 
 CREATE  TABLE nyc_resturant_inspection.dim_location ( 
 	location_id          INT  NOT NULL  ,
-	building             VARCHAR(255)    ,
-	street               VARCHAR(255)    ,
+	building             varchar(255)    ,
+	street               varchar(255)    ,
 	zipcode              INT    ,
-	borough              VARCHAR(20)    ,
-	latitude             DECIMAL(20,20)    ,
-	longitude            DECIMAL(20,20)    ,
+	borough              varchar(20)    ,
+	latitude             numeric(20,20)    ,
+	longitude            numeric(20,20)    ,
 	CONSTRAINT pk_dim_location PRIMARY KEY ( location_id )
  );
 
 CREATE  TABLE nyc_resturant_inspection.dim_resturant ( 
 	resturant_id         INT  NOT NULL  ,
-	resturant_name       VARCHAR(255)    ,
+	resturant_name       varchar(255)    ,
 	phone                INT    ,
-	cuisine_description  VARCHAR(255)    ,
+	cuisine_description  varchar(255)    ,
 	CONSTRAINT pk_dim_resturant PRIMARY KEY ( resturant_id )
  );
 
 CREATE  TABLE nyc_resturant_inspection.dim_violation ( 
 	violation_id         INT  NOT NULL  ,
-	violation_code       TEXT  NOT NULL  ,
-	violation_description VARCHAR(255)    ,
+	violation_code       text  NOT NULL  ,
+	violation_description varchar(255)    ,
 	CONSTRAINT pk_dim_violation PRIMARY KEY ( violation_id )
  );
 
@@ -58,6 +58,6 @@ CREATE  TABLE nyc_resturant_inspection.resturant_facts (
 	inspection_id        INT  NOT NULL  ,
 	inspection_date_id   INT  NOT NULL  ,
 	violation_id         INT  NOT NULL  ,
-	violation_date_id    INT  NOT NULL  ,
+	grade_date_id        INT  NOT NULL  ,
 	CONSTRAINT pk_resturant_facts PRIMARY KEY ( fact_id )
  );
